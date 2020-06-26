b5b1eac152cdb45d2415c90862f55dfc926eff37
muralidharan-rade <muralidharan.rade@gmail.com>
2020-06-26 15:16:47 +0530

    Hateoas change for Hypermedia linking

--- src/main/java/com/nativejava/exceptions/CustomResponseExceptionHandler.java
+++ src/main/java/com/nativejava/exceptions/CustomResponseExceptionHandler.java
@@ -12,9 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.nativejava.user.PostInputNotValidException;
 import com.nativejava.user.PostNotFoundException;
-import com.nativejava.user.UserInputNotValidException;
 import com.nativejava.user.UserNotFoundException;
 
 @ControllerAdvice
@@ -36,28 +34,12 @@ public class CustomResponseExceptionHandler extends ResponseEntityExceptionHandl
 		return new ResponseEntity(exceptionBean, HttpStatus.NOT_FOUND);
 	}
 
-	@ExceptionHandler(UserInputNotValidException.class)
-	public final ResponseEntity<Object> handleUserInputNotValidException(UserInputNotValidException ex,
-			WebRequest request) throws Exception {
-		ExceptionResponseBean exceptionBean = new ExceptionResponseBean(new Date(), ex.getMessage(),
-				ex.getLocalizedMessage(), HttpStatus.BAD_REQUEST.toString());
-		return new ResponseEntity(exceptionBean, HttpStatus.BAD_REQUEST);
-	}
-
 	@ExceptionHandler(PostNotFoundException.class)
 	public final ResponseEntity<Object> handlePostNotFoundException(PostNotFoundException ex, WebRequest request)
 			throws Exception {
 		ExceptionResponseBean exceptionBean = new ExceptionResponseBean(new Date(), ex.getMessage(),
 				ex.getLocalizedMessage(), HttpStatus.NOT_FOUND.toString());
 		return new ResponseEntity(exceptionBean, HttpStatus.NOT_FOUND);
-	}
-
-	@ExceptionHandler(PostInputNotValidException.class)
-	public final ResponseEntity<Object> handlePostInputNotValidException(PostInputNotValidException ex,
-			WebRequest request) throws Exception {
-		ExceptionResponseBean exceptionBean = new ExceptionResponseBean(new Date(), ex.getMessage(),
-				ex.getLocalizedMessage(), HttpStatus.BAD_REQUEST.toString());
-		return new ResponseEntity(exceptionBean, HttpStatus.BAD_REQUEST);
 	}
 
 	@Override
--- src/main/java/com/nativejava/user/UserResource.java
+++ src/main/java/com/nativejava/user/UserResource.java
@@ -1,11 +1,16 @@
 package com.nativejava.user;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import java.net.URI;
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,11 +32,18 @@ public class UserResource {
 	}
 
 	@GetMapping("/users/{userId}")
-	public User getUser(@PathVariable int userId) {
+	public EntityModel<User> getUser(@PathVariable int userId) {
 		User user = service.getUserById(userId);
 		if (user == null)
 			throw new UserNotFoundException(UserConstants.USER_NOT_FOUND + " : " + userId);
-		return user;
+		
+		//hateoas
+
+		EntityModel<User> userEntity = EntityModel.of(user);
+		WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).getAllUserPosts(userId));
+		userEntity.add(linkTo.withRel("user-posts"));
+		
+		return userEntity;
 	}
 
 	@PostMapping("/users")
@@ -41,32 +53,31 @@ public class UserResource {
 				.toUri();
 		return ResponseEntity.created(location).build();
 	}
-	
+
 	@GetMapping("/users/{userId}/posts")
 	public List<Post> getAllUserPosts(@PathVariable int userId) {
 		return service.findAllPosts(userId);
 	}
-	
+
 	@GetMapping("/users/{userId}/posts/{postId}")
 	public Post getUserPost(@PathVariable int userId, @PathVariable int postId) {
 		Post post = service.findPost(userId, postId);
-		if(post == null) {
+		if (post == null) {
 			throw new PostNotFoundException(UserConstants.POST_NOT_FOUND + " : " + postId);
 		}
 		return post;
 	}
-	
+
 	@PostMapping("/users/{userId}/posts")
 	public ResponseEntity<Object> createUserPost(@Valid @RequestBody Post post, @PathVariable int userId) {
 		post.setUserId(userId);
 		Post savedPost = service.addPost(post);
-		
-		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedPost.getPostId())
-				.toUri();
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(savedPost.getPostId()).toUri();
 		return ResponseEntity.created(location).build();
 	}
-	
-	
+
 	@DeleteMapping("/users/{userId}")
 	public User deleteUser(@PathVariable int userId) {
 		User user = service.deleteUserById(userId);
